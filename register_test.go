50b54b75293426d325678f1369cbae09cca8e8f3
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2023-07-13 10:12:36 +0100

    fix(test): MustRegisterDelayedFunc()

--- register_test.go
+++ register_test.go
@@ -1,11 +1,12 @@
 package apphostgae
 
 import (
+	"context"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestMustRegisterDelayedFunc(t *testing.T) {
-	f := MustRegisterDelayedFunc("testFunc", func() {})
+	f := MustRegisterDelayedFunc("testFunc", func(_ context.Context) {})
 	assert.NotNil(t, f)
 }
